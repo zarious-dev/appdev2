0453ca5a4df37b552d74e9b8064694b98f7f18c0
zarious-dev <enriquez.caezarie@gmail.com>
2026-02-13 12:46:31 +0800

    Done with align items section

--- react-native-flexbox/App.js
+++ react-native-flexbox/App.js
@@ -1,18 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
 // import Flex from './assets/components/Flex';
 // import FlexDirectionBasics from './assets/components/FlexDirectionBasics';
 // import DirectionLayout from './assets/components/DirectionLayout';
-import JustifyContentBasics from './assets/components/JustifyContentBasics';
+// import JustifyContentBasics from './assets/components/JustifyContentBasics';
+import AlignItemsLayout from './assets/components/AlignItemsLayout';
 
 export default function App() {
   return (
-    <>
+    <View style={{ flex: 1 }}>
       {/* <Flex /> */}
       {/* <FlexDirectionBasics /> */}
       {/* <DirectionLayout/> */}
-      <JustifyContentBasics/>
+      {/* <JustifyContentBasics/> */}
+      <AlignItemsLayout/>
       <StatusBar />
-    </>
+    </View>
   );
 }
 
