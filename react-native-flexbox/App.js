c0a8d88599b2e214ec8df9c1f573d4bb4c81bf4b
zarious-dev <enriquez.caezarie@gmail.com>
2026-02-13 12:20:10 +0800

    Done with layout direction section

--- react-native-flexbox/App.js
+++ react-native-flexbox/App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 // import Flex from './assets/components/Flex';
-import FlexDirectionBasics from './assets/components/FlexDirectionBasics';
+// import FlexDirectionBasics from './assets/components/FlexDirectionBasics';
+import DirectionLayout from './assets/components/DirectionLayout';
 
 export default function App() {
   return (
     <>
       {/* <Flex /> */}
-      <FlexDirectionBasics />
+      {/* <FlexDirectionBasics /> */}
+      <DirectionLayout/>
       <StatusBar />
     </>
   );
