2db3ad62d227cf295a6e56cb70e65a97e70be25a
zarious-dev <enriquez.caezarie@gmail.com>
2026-02-13 12:28:00 +0800

    Done with justify content section

--- react-native-flexbox/App.js
+++ react-native-flexbox/App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 // import Flex from './assets/components/Flex';
 // import FlexDirectionBasics from './assets/components/FlexDirectionBasics';
-import DirectionLayout from './assets/components/DirectionLayout';
+// import DirectionLayout from './assets/components/DirectionLayout';
+import JustifyContentBasics from './assets/components/JustifyContentBasics';
 
 export default function App() {
   return (
     <>
       {/* <Flex /> */}
       {/* <FlexDirectionBasics /> */}
-      <DirectionLayout/>
+      {/* <DirectionLayout/> */}
+      <JustifyContentBasics/>
       <StatusBar />
     </>
   );
