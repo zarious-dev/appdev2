fbdafffc3314ef424f50634d04af511953b95985
zarious-dev <enriquez.caezarie@gmail.com>
2026-02-13 11:52:28 +0800

    Done with flex section

---
+++ react-native-flexbox/App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import Flex from './assets/components/Flex';
+
+export default function App() {
+  return (
+    <>
+      <Flex />
+      <StatusBar />
+    </>
+
+  );
+}
