b130a25a5ed5febae7c83ba103d83c86c86332e5
zarious-dev <enriquez.caezarie@gmail.com>
2026-04-17 13:06:07 +0800

    Done with navigate to a screen multiple times section

--- react-native-navigation/App.tsx
+++ react-native-navigation/App.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
-import {
-  createStaticNavigation,
-  useNavigation,
-} from '@react-navigation/native';
+import { createStaticNavigation, useNavigation, } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Button } from '@react-navigation/elements';
 
@@ -21,9 +18,14 @@ function HomeScreen() {
 }
 
 function DetailsScreen() {
+  const navigation = useNavigation();
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Button onPress={() => navigation.navigate('Details')}>
+        Go to Details... again
+      </Button>
     </View>
   );
 }
