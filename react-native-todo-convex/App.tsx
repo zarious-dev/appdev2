105ac37d5bb1a24fab96c5a439854b9b3222e3b6
zarious-dev <enriquez.caezarie@gmail.com>
2026-04-24 13:28:58 +0800

    Done with copy App code section

--- react-native-todo-convex/App.tsx
+++ react-native-todo-convex/App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import TodoScreen from "./screens/TodoScreen";
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ConvexProvider client={convex}>
+      <TodoScreen />
+    </ConvexProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
